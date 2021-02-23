81cf31cc4bf5b068876ada37f8fad4f07a767773
Bartosz Kopeć <kopec.bartosz.app@gmail.com>
2021-02-23 15:38:03 +0100

    Project init

---
+++ src/Application/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application
+{
+	internal struct Constants
+	{
+		public const string ROUTE_API_RSS = "api/rss";
+		public const string ROUTE_API_MAILING = "api/mailing";
+		public const string CONTENTTYPE_JSON = "application/json";
+	}
+}
---
+++ src/Application/Controllers/RssController.cs
@@ -0,0 +1,27 @@
+﻿using Application.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Controllers
+{
+	[Route(Constants.ROUTE_API_RSS)]
+	[ApiController]
+	[Produces(Constants.CONTENTTYPE_JSON)]
+	public class RssController : ControllerBase
+	{
+		[HttpGet]
+		public async Task<IActionResult> GetList([FromQuery]string email, CancellationToken token=default)
+		{
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> InsertRss([FromBody]RssRequestBody body, CancellationToken token=default)
+		{
+		}
+	}
+}
